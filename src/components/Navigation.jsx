2842871244266de9f1429f5fd039f193c9a9e809
VISHAL RAJPUT <Vishal Rajput>
2025-04-04 21:52:39 +0530

    second

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,24 @@
 export default function Navigation() {
-    return (
-      <nav className="container">
-        <div className="logo">
-          <img src="/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">Location</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <button className="primary-btn">Login</button>
-      </nav>
-    );
-  }
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Location</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+      <button>Login</button>
+    </nav>
+  );
+}
